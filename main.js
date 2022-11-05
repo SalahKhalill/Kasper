42de06cdf8fe197d159bc2566432c901bbe1199b
Salah Khalil <108340266+SalahKhalill@users.noreply.github.com>
2022-11-05 21:12:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-let tabs = document.querySelectorAll(".portfolio .container .shuffle li");
-let tabsArray = Array.from(tabs)
-let divs = document.querySelectorAll(".imgs-container > div")
-let divsArray = Array.from(divs)
-let active = document.getElementById("#li1")
-let allDivs= document.querySelector(".imgs-container")
-
-tabsArray.forEach((ele) => {
-    ele.addEventListener("click", function (e) {
-        tabsArray.forEach((ele) => {
-        ele.classList.remove("active");
-        });
-        e.currentTarget.classList.add("active");
-        divsArray.forEach((div) => {
-            div.style.display = "none";
-         }); 
-        //  console.log(e.currentTarget.dataset.cont);
-        document.querySelector(currentTarget.dataset.cont).style.display = "block";
-    });
-});
-
-
 
 //up btn
 let btn = document.getElementById("upb");
